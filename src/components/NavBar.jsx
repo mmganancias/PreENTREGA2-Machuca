884f438ce7d13875a826454280177eeddc5f2df6
marcelamachucaganancias <address>
2023-06-12 20:00:35 -0300

    primer commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+export const NavBar = () => (
+  <header>
+    <h1>GINGER</h1>
+    <nav>
+      <a href="">Ginger</a>
+      <a href="">Accesorios</a>
+      <a href="">Blog</a>
+      <a href="">Contacto</a>
+      <a href="">Ropa Interior</a>
+      <a href="">Sobre Nosotras</a>
+    </nav>
+  </header>
+);
